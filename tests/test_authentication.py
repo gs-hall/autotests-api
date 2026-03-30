7e409d99ba62632137dbe42bcd20965798b42cd6
gs-hall <egorivantsov@gmail.com>
2026-03-30 18:52:47 +0300

    test_authentication.py

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+import pytest
+
 from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
 from clients.authentication.authentication_client import get_authentication_client
 from clients.users.public_users_client import get_public_users_client
@@ -8,6 +10,8 @@ from tools.assertions.base import assert_status_code
 from tools.assertions.authentication import assert_login_response
 
 
+@pytest.mark.regression
+@pytest.mark.authentication
 def test_login():
     """
     Тест проверяет процесс аутентификации пользователя.
