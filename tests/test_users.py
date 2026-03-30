7e60695c225a4e62025165cc7c31d92e5e9a2636
gs-hall <egorivantsov@gmail.com>
2026-03-31 00:21:25 +0300

    test_create_user, Fake.email

--- tests/test_users.py
+++ tests/test_users.py
@@ -2,10 +2,11 @@ from http import HTTPStatus
 
 import pytest
 
-from clients.users.public_users_client import PublicUsersClient
 from clients.users.private_users_client import PrivateUsersClient
+from clients.users.public_users_client import PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
-from tests.conftest import UserFixture
+from fixtures.users import UserFixture  # Заменяем импорт
+from tools.fakers import fake
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.users import assert_create_user_response, assert_get_user_response
@@ -13,9 +14,10 @@ from tools.assertions.users import assert_create_user_response, assert_get_user_
 
 @pytest.mark.users
 @pytest.mark.regression
-def test_create_user(public_users_client: PublicUsersClient):  # Используем фикстуру API клиента
+@pytest.mark.parametrize("email", ["mail.ru", "gmail.com", "example.com"])
+def test_create_user(email: str, public_users_client: PublicUsersClient):  # Используем фикстуру API клиента
     # Удалили инициализацию API клиента из теста
-    request = CreateUserRequestSchema()
+    request = CreateUserRequestSchema(email=fake.email(domain=email))
     response = public_users_client.create_user_api(request)
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
