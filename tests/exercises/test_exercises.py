3247d550cca2b37039ab39e6da0b3b09d2c7167b
gs-hall <egorivantsov@gmail.com>
2026-03-31 20:53:35 +0300

    test_get_exercise

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -1,10 +1,10 @@
 import pytest
 from http import HTTPStatus
 from clients.exercises.exercises_client import ExercisesClient
-from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, GetExerciseResponseSchema
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
-from tools.assertions.exercises import assert_create_exercise_response
+from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response
 
 @pytest.mark.exercises
 @pytest.mark.regression
@@ -22,4 +22,16 @@ class TestExercises:
 
         assert_status_code(response.status_code, HTTPStatus.OK)
         assert_create_exercise_response(request, response_data)
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_get_exercise(self, exercises_client, function_exercise):
+        """
+        Проверяет получение задания через GET /api/v1/exercises/{exercise_id}.
+        """
+        exercise_id = function_exercise.response.exercise.id
+        response = exercises_client.get_exercise_api(exercise_id)
+        response_data = GetExerciseResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_get_exercise_response(response_data, function_exercise.response)
         validate_json_schema(response.json(), response_data.model_json_schema())
